a75aa6ee4a1bad30c88566d098077b381bc1fe89
ashish <ashishtayde.tayde@gmail.com>
2018-02-18 11:27:28 +0530

    add sample method

--- MyApplication/app/src/main/java/com/example/admin/myapplication/TestClass.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/TestClass.java
@@ -1,9 +1,12 @@
 package com.example.admin.myapplication;
 
+import android.widget.Toast;
+
 /**
  * Created by admin on 2/18/2018.
  */
 
 public class TestClass {
-    
+
+    public void test(){}
 }
